5a936d263ccd29309e0634c3d4c0ab99a1d0b4a7
Your Name <you@example.com>
2024-04-23 15:40:06 +0200

    setup the pages

---
+++ src/layouts/loggedLayout.vue
@@ -0,0 +1,41 @@
+import { import { UserCircle } from '@heroicons/vue/24/solid';
+} from '@heroicons/vue/24/solid';
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Weather Station
+
+        </q-toolbar-title>
+        <q-icon >  </q-icon>
+      </q-toolbar>
+      <q-tabs align="center">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+<script setup lang="js">
+</script>

499defecac2c2ba1c2780f82dc7b34319b1be6b2
Your Name <you@example.com>
2024-04-24 17:45:51 +0200

    :zap: implementazione props

--- src/components/cardComponent.vue
+++ src/components/cardComponent.vue
@@ -1,33 +1,36 @@
 <script setup>
-import {ref} from 'vue';
-const cardTitle = ref('titolone');
+import {ref } from 'vue';
 
 const hello = () => {
   console.log('Hello');
 }
+  defineProps({
+    city:{}
+  })
+
 </script>
 
 <template>
       <q-card class="my-card">
         <q-card-section class="bg-grey-8 text-white">
-          <div class="text-h6"> {{cardTitle}} </div>
+          <div class="text-h6"> {{city.name}} </div>
           <div class="absolute-bottom text-subtitle2 text-center">
-            {{ cardSubTitle }}
+            {{ city.degrees }}°
           </div>
         </q-card-section>
 
         <q-card-actions vertical align="center">
-          <q-btn flat @click="hello()"> azione 1 </q-btn> <!-- inserire l'icona della meteora con l'animazione -->
+          <q-icon> </q-icon> <!-- inserire l'icona dipendentemente da se piove o no-->
         </q-card-actions>
       </q-card>
 </template>
 
 <style scoped>
 .my-card{
+  flex-wrap: wrap;
   width: 25%;
   margin:3%;
   border-radius: 12px;
-  float: left;
   height: 22vb;
 }
 </style>
--- src/pages/favouritePage.vue
+++ src/pages/favouritePage.vue
@@ -1,11 +1,32 @@
 <script setup>
 import cardComponent from 'components/cardComponent.vue'
 
+const arrayCity = [
+  {
+    name: 'Bergamo',
+    degrees: 19,
+    precipitation: 2,
+  },
+  {
+    name: 'Brescia',
+    degrees: 5,
+    precipitation: 0,
+  },
+  {
+    name: 'Cene',
+    degrees: 1.4,
+    precipitation: 3,
+  }
+]
 </script>
 
 <template>
   <div class="q-pa-md cardContainer">
-
+    <q-list>
+      <q-item v-for="index in arrayCity.length" :key="index">
+        <card-component :city="arrayCity[index-1]"> </card-component>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
